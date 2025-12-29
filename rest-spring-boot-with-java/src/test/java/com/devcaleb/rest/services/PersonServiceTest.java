403dfedebf5e17cb01e15a09f142d3f144f28295
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-12-29 17:51:40 +0200

    Testing the generation of the links HATEOAS on the Service winth Mockito

---
+++ rest-spring-boot-with-java/src/test/java/com/devcaleb/rest/services/PersonServiceTest.java
@@ -0,0 +1,103 @@
+package com.devcaleb.rest.services;
+
+import com.devcaleb.rest.model.Person;
+import com.devcaleb.rest.repositories.PersonRepository;
+import com.devcaleb.rest.unitetests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServiceTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonService service;
+
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+        var result = service.findById(1L);
+
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("self")
+                && link.getHref().endsWith("api/person/v1/1")
+                && link.getType().equals("GET"))
+
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("findAll")
+                && link.getHref().endsWith("api/person/v1/people")
+                && link.getType().equals("GET"))
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("insert")
+                && link.getHref().endsWith("api/person/v1")
+                && link.getType().equals("POST"))
+
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("update")
+                && link.getHref().endsWith("api/person/v1")
+                && link.getType().equals("PUT"))
+        );
+
+        assertNotNull(result.getLinks().stream()
+                .anyMatch(link -> link.getRel().value().equals("delete")
+                && link.getHref().endsWith("api/person/v1/1")
+                && link.getType().equals("DELETE"))
+        );
+
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
