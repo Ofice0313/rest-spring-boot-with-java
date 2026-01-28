f4e930ecbeda038764e1330df58622611fd3916d
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-01-28 11:06:22 +0200

    Aplicando CORS de forma global

--- rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/config/WebConfig.java
+++ rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/config/WebConfig.java
@@ -1,26 +1,31 @@
 package com.devcaleb.rest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                //.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedMethods("*")
+                .allowCredentials(true);
+
+    }
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-
-        //Via Extension. localhost:8080/api/person/2.xml or localhost:8080/api/person/2.JSON
-        // Via QUERY PARAM localhost:8080/api/person/2?mediaType=xml
-        /*configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
-                .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
-        */
 
         configurer.favorParameter(false)
                 .ignoreAcceptHeader(false)
--- rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/controllers/PersonController.java
+++ rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/controllers/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/person/v1/")
 @Tag(name = "People", description = "Endpoints for managing People")
@@ -28,6 +29,7 @@ public class PersonController implements PersonControllerDocs {
         return service.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping(value = "/{id}",
             produces = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
@@ -38,6 +40,7 @@ public class PersonController implements PersonControllerDocs {
         return person;
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
