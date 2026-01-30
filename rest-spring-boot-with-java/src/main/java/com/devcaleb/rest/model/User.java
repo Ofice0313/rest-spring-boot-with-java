2735c54c10aa112880053c9618fbc895a9161a2c
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-01-31 00:31:15 +0200

    Entity User and Permission, remoção do flyway e migrations

---
+++ rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/model/User.java
@@ -0,0 +1,95 @@
+package com.devcaleb.rest.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_name", unique = true)
+    private String userName;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "account_non_expired")
+    private Boolean accountNonExpired;
+    @Column(name = "account_non_locked")
+    private Boolean accountNonLocked;
+    @Column(name = "credentials_non_expired")
+    private Boolean credentialsNonExpired;
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "permission_id")}
+    )
+    private List<Permission> permissionsList;
+
+    public List<String> getRoles () {
+        List<String> roles = new ArrayList<>();
+        for(Permission permission: permissionsList) {
+            roles.add(permission.getDescription());
+        }
+        return roles;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String fullName, String password, Boolean accountNonExpired, Boolean accountNonLocked, Boolean credentialsNonExpired, Boolean enabled) {
+        this.id = id;
+        this.userName = userName;
+        this.fullName = fullName;
+        this.password = password;
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissionsList;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
