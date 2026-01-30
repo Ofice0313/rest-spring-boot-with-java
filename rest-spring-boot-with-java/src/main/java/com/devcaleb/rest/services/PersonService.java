145fd5e702f2d231b1e5abb4e7bc5017cc5e6809
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-01-30 02:45:52 +0200

    Direction

--- rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/services/PersonService.java
+++ rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/services/PersonService.java
@@ -101,7 +101,7 @@ public class PersonService {
 
     private static void addHateoasLinks(PersonDTO dto) {
         dto.add(linkTo(methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
-        dto.add(linkTo(methodOn(PersonController.class).findAll(1, 12)).withRel("findAll").withType("GET"));
+        dto.add(linkTo(methodOn(PersonController.class).findAll(1, 12, "asc")).withRel("findAll").withType("GET"));
         dto.add(linkTo(methodOn(PersonController.class).insert(dto)).withRel("insert").withType("POST"));
         dto.add(linkTo(methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
         dto.add(linkTo(methodOn(PersonController.class).disablePerson(dto.getId())).withRel("disable").withType("PATCH"));
