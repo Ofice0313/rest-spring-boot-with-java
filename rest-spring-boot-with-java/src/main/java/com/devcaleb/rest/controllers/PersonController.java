43be06b4939ae201b266796e76e85a58c2f21709
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-02-03 09:35:41 +0200

    All Alterations

--- rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/controllers/PersonController.java
+++ rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/controllers/PersonController.java
@@ -5,7 +5,6 @@ import com.devcaleb.rest.data.dto.v1.PersonDTO;
 import com.devcaleb.rest.services.PersonService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
--- rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/services/PersonService.java
+++ rest-spring-boot-with-java/src/main/java/com/devcaleb/rest/services/PersonService.java
@@ -54,7 +54,8 @@ public class PersonService {
                                 pageable.getPageSize(),
                                 String.valueOf(pageable.getSort())))
                         .withSelfRel();
-        return assembler.toModel(peopleWithLinks, findAllLink);
+        return assembler.toModel(peopleWithLinks,
+                findAllLink);
     }
 
     public PagedModel<EntityModel<PersonDTO>> findByName(String firstName, Pageable pageable) {
